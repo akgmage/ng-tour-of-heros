88b0ca2a9a73b99be7932245114b39d4e3039181
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-13 15:59:11 +0530

    display a history of each time the user clicks on a hero

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
@@ -10,7 +11,10 @@ export class HerosComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -18,6 +22,7 @@ export class HerosComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
