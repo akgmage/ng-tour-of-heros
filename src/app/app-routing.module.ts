2291e823f36bf3b08e31a003fb129e8316ae9ca2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-14 16:12:41 +0530

    use HeroComponent in routes array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HerosComponent } from './heros/heros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HerosComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
