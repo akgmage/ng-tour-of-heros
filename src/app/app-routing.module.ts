88fd499ed243a36685f90dcb51b9b30d68ea2677
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-14 16:27:40 +0530

    add dashbaord route and a default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HerosComponent } from './heros/heros.component';
 
 const routes: Routes = [
   {
     path: 'heroes',
     component: HerosComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
   },
 ];
 
