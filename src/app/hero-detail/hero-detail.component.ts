39d906613b3dbbdae51e2ad0face593333cc639a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-15 13:54:13 +0530

    add a goBack() method to the component class that navigates backward one step in the browser's history stack using the injected Location service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,6 +23,9 @@ export class HeroDetailComponent implements OnInit {
   }
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHeroes(id).subscribe((hero) => (this.hero = hero));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+  goBack(): void {
+    this.location.back();
   }
 }
